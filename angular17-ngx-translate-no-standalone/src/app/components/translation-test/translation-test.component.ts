ce0e669b665e8a6e08b340955300023536b7e054
OliverKuhnNbg <oliver.o.kuhn@googlemail.com>
2024-05-01 21:19:41 +0200

    add translation test component

---
+++ angular17-ngx-translate-no-standalone/src/app/components/translation-test/translation-test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-translation-test',
+  templateUrl: './translation-test.component.html',
+  styleUrl: './translation-test.component.scss'
+})
+export class TranslationTestComponent {
+
+}
